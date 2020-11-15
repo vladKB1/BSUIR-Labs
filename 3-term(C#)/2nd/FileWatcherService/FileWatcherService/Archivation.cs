bb900e7c356b68e87eea71e39c40cf26f263dcff
vladKB1 <kazlou.uladzislau@gmail.com>
2020-11-15 21:47:50 +0300

    2nd lab is done

---
+++ 3-term(C#)/2nd/FileWatcherService/FileWatcherService/Archivation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcherService
+{
+    static class Archivation
+    {
+        public static void Compress(string src, string arhcivePath)
+        {
+            using (FileStream sourceStream = new FileStream(src, FileMode.OpenOrCreate))
+            {
+                using (FileStream targetStream = File.Create(arhcivePath))
+                {
+                    using (GZipStream compressStream = new GZipStream(targetStream, CompressionMode.Compress))
+                    {
+                        sourceStream.CopyTo(compressStream);
+                    }
+                }              
+            }
+        }
+
+        public static void Decompress(string arhcivePath, string target)
+        {
+            using (FileStream sourceStream = new FileStream(arhcivePath, FileMode.OpenOrCreate))
+            {
+                using (FileStream targetStream = File.Create(target))
+                {
+                    using (GZipStream decompressStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                    {
+                        decompressStream.CopyTo(targetStream);
+                    }
+                }
+            }
+        }        
+    }
+}
---
+++ 3-term(C#)/2nd/FileWatcherService/FileWatcherService/Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileWatcherService
+{
+    public partial class Service : ServiceBase
+    {
+        Watcher watcher;        
+
+        public Service()
+        {
+            InitializeComponent();
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            watcher = new Watcher();
+            Thread watcherThread = new Thread(new ThreadStart(watcher.Start));
+            watcherThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            watcher.Stop();
+            Thread.Sleep(1000);
+        }
+    }
+}
