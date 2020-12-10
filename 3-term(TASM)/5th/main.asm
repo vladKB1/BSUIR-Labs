2d2cfbbdf2a4c5f3c8c6712ca0b1b817dce57da4
vladKB1 <kazlou.uladzislau@gmail.com>
2020-12-10 23:35:27 +0300

    feat: add Floyd's algorithm and finish the 5th lab

--- 3-term(TASM)/5th/main.asm
+++ 3-term(TASM)/5th/main.asm
@@ -11,7 +11,9 @@ model small
 	elementSize dw 1
 
 	n db 0
-	ms db 10 dup(10 dup(0))
+	ms db 10 dup(10 dup(0))	
+	ki dw 0
+	kj dw 0
 .code
 .386
 
@@ -34,6 +36,9 @@ readMatrix proc
 			int 21h
 
 			cmp al, entr
+			je nextStepForj1
+
+			cmp al, new_line
 			je nextStepForj1
 
 			cmp al, space
@@ -84,34 +89,38 @@ printMatrix proc
 		mov cx, dx
 		dec cx
 		mov si, 0
-		
-		push dx
+
+		cmp cx, 0
+		je nextStepFori2
 
 		forj2:
 			xor ax, ax
 			mov al, [bx][si]
 			call printAX
 
+			push dx
 			mov ah, 02h
 			mov dl, space
 			int 21h
+			pop dx
 			
 			add si, elementSize
 		loop forj2
 
-		
+		nextStepFori2:
 		xor ax, ax
 		mov al, [bx][si]
 		call printAX
 		
+		push dx
 		mov ah, 02h
 		mov dl, new_line
 		int 21h
+		pop dx
 
 		add si, elementSize
 
 
-		pop dx
 		pop cx
 		add bx, si
 	loop fori2
@@ -172,6 +181,9 @@ readToAX proc
 		cmp al, entr
 		je findEnter
 
+		cmp al, new_line
+		je findEnter
+
 		xor ah, ah		
 		sub al, '0'
 		xor ax, bx
@@ -206,6 +218,54 @@ main:
 	call readMatrix
 
 
+	mov ki, 0
+	mov kj, 0
+	fork3:
+		push cx
+		
+		xor cx, cx
+		mov cl, n
+		mov bx, 0
+		fori3:
+			push cx
+
+			xor cx, cx
+			mov cl, n
+			mov si, 0
+			forj3:
+				push si
+				mov si, kj
+				mov al, ms[bx][si]
+				pop si
+				push bx
+				mov bx, ki
+				add al, ms[bx][si]
+				pop bx
+
+				cmp ms[bx][si], al
+				ja newAns
+				
+				jmp nextStepForj3
+
+
+				newAns:
+				mov ms[bx][si], al
+				
+
+				nextStepForj3:
+				add si, elementSize
+			loop forj3
+
+			pop cx
+			add bx, si						
+		loop fori3
+
+		pop cx
+		add ki, si
+		mov ax, kj
+		add ax, elementSize
+		mov kj, ax
+	loop fork3
 	
 
 	lea bx, ms
