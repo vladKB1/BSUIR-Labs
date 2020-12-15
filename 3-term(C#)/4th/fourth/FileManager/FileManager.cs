6415a4d7b0e8d01f5047e84ea92450f532b0c0b4
vladKB1 <kazlou.uladzislau@gmail.com>
2020-12-15 10:45:45 +0300

    init: 4th lab is done

--- 3-term(C#)/4th/fourth/FileManager/FileManager.cs
+++ 3-term(C#)/4th/fourth/FileManager/FileManager.cs
@@ -18,18 +18,18 @@ namespace FileManager
         Logger.Logger logger;
         OptionsManager.OptionsManager<ETLOptions> optionsManager;
         Validator validator;
-        Parser.Parser parser;       
+        Parser.Parser parser;
 
         string source = "";
         string target = "";
-        string saveArchive = "";               
+        string saveArchive = "";
 
         public FileManager()
-        {             
+        {
             string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
             validator = new Validator();
             parser = new Parser.Parser();
-            optionsManager = 
+            optionsManager =
                 new OptionsManager.OptionsManager<ETLOptions>(appDirectory, parser, validator);
             ETLOptions options = optionsManager.GetOptions<ETLOptions>() as ETLOptions;
 
@@ -38,11 +38,11 @@ namespace FileManager
             saveArchive = options.ArchivationOptions.ArchiveDir;
 
             logger = new Logger.Logger(optionsManager.GetOptions<Logger.LoggerOptions>()
-                as Logger.LoggerOptions);
-            logger.Log(optionsManager.LogString);                       
+                 as Logger.LoggerOptions);
+            logger.Log(optionsManager.LogString);
 
             watcher = new FileSystemWatcher(source);
-            watcher.Created += Created;           
+            watcher.Created += Created;
         }
 
         public void Start()
@@ -52,7 +52,7 @@ namespace FileManager
             while (enabled)
             {
                 Thread.Sleep(1000);
-            }            
+            }
         }
 
         public void Stop()
@@ -63,61 +63,73 @@ namespace FileManager
         }
 
         private void Created(object sender, FileSystemEventArgs e)
-        {     
-            string pathToFile = e.FullPath;
-            DateTime date = File.GetLastWriteTime(pathToFile);
-            string name = Path.GetFileNameWithoutExtension(pathToFile);
-            string extansion = Path.GetExtension(pathToFile);
-
-            EncryptionOptions encryptionOptions = 
-                optionsManager.GetOptions<EncryptionOptions>() as EncryptionOptions;
-            ArchivationOptions archivationOptions =
-                optionsManager.GetOptions<ArchivationOptions>() as ArchivationOptions;
-
-
-            if (extansion != ".gz" && extansion != "")
+        {
+            try
             {
-                byte[] key, iv;
-                (key, iv) = Encryption.GenKeyIv();
+                string pathToFile = e.FullPath;
+                DateTime date = File.GetLastWriteTime(pathToFile);
+                string name = Path.GetFileNameWithoutExtension(pathToFile);
+                string extansion = Path.GetExtension(pathToFile);
 
-                if (encryptionOptions.NeedToEncrypt)
-                {                    
-                    File.WriteAllBytes(pathToFile, Encryption.Encrypt(pathToFile, key, iv));
-                }
-                
-                string pathToArchive = Path.Combine(source, name + ".gz");
-                Archivation.Compress(pathToFile, pathToArchive, archivationOptions);
+                EncryptionOptions encryptionOptions =
+                    optionsManager.GetOptions<EncryptionOptions>() as EncryptionOptions;
+                ArchivationOptions archivationOptions =
+                    optionsManager.GetOptions<ArchivationOptions>() as ArchivationOptions;
 
 
-                File.Delete(pathToFile);
 
-                if (!Directory.Exists(saveArchive))
+                if (extansion != ".gz" && extansion != "")
                 {
-                    Directory.CreateDirectory(saveArchive);
+                    byte[] key, iv;
+                    (key, iv) = Encryption.GenKeyIv();
+
+                    if (encryptionOptions.NeedToEncrypt)
+                    {
+                        File.WriteAllBytes(pathToFile, Encryption.Encrypt(pathToFile, key, iv));
+                    }
+
+                    string pathToArchive = Path.Combine(source, name + ".gz");
+                    Archivation.Compress(pathToFile, pathToArchive, archivationOptions);
+
+
+                    File.Delete(pathToFile);
+
+                    if (!Directory.Exists(saveArchive))
+                    {
+                        Directory.CreateDirectory(saveArchive);
+                    }
+
+                    string newPathToArchive = Path.Combine(saveArchive, name + ".gz");
+                    if (File.Exists(newPathToArchive))
+                    {
+                        File.Delete(newPathToArchive);
+                    }
+                    File.Move(pathToArchive, newPathToArchive);
+
+
+                    string newPathToFile = Path.Combine(target, date.Year.ToString(),
+                        date.Month.ToString(), date.Day.ToString());
+                    Directory.CreateDirectory(newPathToFile);
+
+                    newPathToFile = Path.Combine(newPathToFile, name + "_"
+                        + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss") + extansion);
+                    Archivation.Decompress(newPathToArchive, newPathToFile);
+
+                    if (encryptionOptions.NeedToEncrypt)
+                    {
+                        File.WriteAllBytes(newPathToFile, Encryption.Decrypt(newPathToFile, key, iv));
+                    }
                 }
-
-                string newPathToArchive = Path.Combine(saveArchive, name + ".gz");
-                if (File.Exists(newPathToArchive))
-                {
-                    File.Delete(newPathToArchive);
-                }
-                File.Move(pathToArchive, newPathToArchive);
-
-
-                string newPathToFile = Path.Combine(target, date.Year.ToString(),
-                    date.Month.ToString(), date.Day.ToString());
-                Directory.CreateDirectory(newPathToFile);
-
-                newPathToFile = Path.Combine(newPathToFile, name + "_"
-                    + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss") + extansion);
-                Archivation.Decompress(newPathToArchive, newPathToFile);
-
-                if (encryptionOptions.NeedToEncrypt)
-                {
-                    File.WriteAllBytes(newPathToFile, Encryption.Decrypt(newPathToFile, key, iv));
-                }
-            }                                      
+            }
+            catch (IOException ex)
+            {
+                Thread.Sleep(1000);
+                Created(sender, e);
+            }
+            catch (Exception ex)
+            {
+                logger.Log(ex.Message);
+            }
         }
     }
 }
-    
